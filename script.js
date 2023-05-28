7f33a52a30fe0f8fc9809c90c2959133124e327b
Matt Moore <mattmoore2014@gmail.com>
2023-05-27 20:51:52 -0600

    error in my api

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = 'd50e8afbf6e44f0ffeeb90c440999819';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
